5e0054ffce6b2fcaebe01f4c5241769d47f4f2df
Roger Patino <rogerp.009@gmail.com>
2016-12-13 10:08:39 -0400

    chore: Change oof key google maps

--- app/src/main/java/com/github/testairbnd/ui/MapsActivity.java
+++ app/src/main/java/com/github/testairbnd/ui/MapsActivity.java
@@ -1,8 +1,9 @@
-package com.github.testairbnd.ui.activity;
+package com.github.testairbnd.ui;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.github.testairbnd.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,7 +18,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps3);
+        setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
